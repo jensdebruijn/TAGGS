5e52b0fcc9bf0d941ddb97f9353e25dcf742b2d0
Jens de Bruijn <j.a.debruijn@outlook.com>
2017-05-24 10:15:14 -0700

    more descriptive naming of variables

--- geotag_run.py
+++ geotag_run.py
@@ -1,11 +1,10 @@
 import datetime
-import geotag_config
 import geotag_geotag
 
 if __name__ == '__main__':
-    geotagger = geotag_geotag.Geotag(0.2, 5000, 1000)
+    geotagger = geotag_geotag.Geotag(min_score=.2, min_population=5000, n_words=1000)
     start = datetime.datetime(2014, 7, 29)
     end = False
     geotagger.history(
-        start, datetime.timedelta(hours=6), datetime.timedelta(hours=24), end=end, realtime=False
+        start, timestep_length=datetime.timedelta(hours=6), analysis_lengthdatetime.timedelta(hours=24), end=end, realtime=False
     )
